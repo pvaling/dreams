6d28e85ebe547fb377528f54aae045eb4b343a79
Petr Valing <petr.valing@gmail.com>
2019-09-04 11:01:15 +0300

    basic models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Dream, Vote, Donation, Video
+
+admin.site.register(Dream)
+admin.site.register(Vote)
+admin.site.register(Donation)
+admin.site.register(Video)
--- core/models.py
+++ core/models.py
@@ -6,24 +6,31 @@ from django.db.models import ForeignKey, DateTimeField, FileField, PROTECT, CASC
 from djmoney.models.fields import MoneyField
 
 
-class UGCMixin(object):
+class UGCMixin(models.Model):
     author = ForeignKey(to=get_user_model(), on_delete=PROTECT)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
 
-class Video(models.Model, UGCMixin):
+
+class Video(UGCMixin):
     file = FileField(upload_to='videos')
 
 
-class Dream(models.Model, UGCMixin):
+class Dream(UGCMixin):
+    title = models.fields.TextField()
     description = models.fields.TextField(null=True, verbose_name='Description')
     budget = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
 
-    media = ForeignKey(to=Video, on_delete=CASCADE)
+    media = ForeignKey(to=Video, null=True, blank=True, on_delete=CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 
-class Vote(models.Model, UGCMixin):
+class Vote(UGCMixin):
     value = models.fields.PositiveSmallIntegerField(
         choices=(
             (1, 'Positive'),
@@ -32,7 +39,10 @@ class Vote(models.Model, UGCMixin):
     )
 
 
-class Donation(models.Model, UGCMixin):
+class Donation(UGCMixin):
     amount = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
     purpose = ForeignKey(to=Dream, on_delete=PROTECT)
     comment = TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.amount} for {self.purpose}"
